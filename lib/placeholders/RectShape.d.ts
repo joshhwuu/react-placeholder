3ea82981ee42b25ccf787ec8f98963d72d00c2e4
joshhwuu <joshuawu2004@gmail.com>
2025-07-30 15:09:03 -0700

    Fix GitHub installation and update to personal account
    - Added prepare script to build package after npm install
    - Removed lib directory from .gitignore
    - Included built lib files in repository
    - Updated package.json with personal GitHub account info
    - Fixes module build errors when installing from GitHub

---
+++ lib/placeholders/RectShape.d.ts
@@ -0,0 +1,8 @@
+import * as React from 'react';
+export type Props = {
+    color?: string;
+    className?: string;
+    style?: React.CSSProperties;
+};
+declare const RectShape: React.FC<Props>;
+export default RectShape;
---
+++ lib/placeholders/RoundShape.d.ts
@@ -0,0 +1,8 @@
+import * as React from 'react';
+export type Props = {
+    color: string;
+    style?: React.CSSProperties;
+    className?: string;
+};
+declare const RoundShape: React.FC<Props>;
+export default RoundShape;
